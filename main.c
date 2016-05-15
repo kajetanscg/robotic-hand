4864d2c489c9686511fb7d95551c2f4108e79762
Gaetano Scognamiglio <g.scognamiglio1124@gmail.com>
2016-05-15 16:41:29 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 * Created on: 06/28/2014
 * Author: Gaetano Scognamiglio
 
-* Copyright© 2016 Gaetano Scognamiglio
+* Copyright© 2014 Gaetano Scognamiglio
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
